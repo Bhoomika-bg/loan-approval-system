f84b56aa261e8b29835aa3abee717f0a0dc20c19
Bhoomika bg <bhoomikabg63@gmail.com>
2026-03-29 20:08:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 st.title("🏦 Loan Approval Predictor")
 
-income       = st.number_input("Applicant Income ($)", 0)
-credit_score = st.number_input("Credit Score", 0)
-dti          = st.number_input("DTI Ratio (%)", 0)
-loans        = st.number_input("Existing Loans", 0)
-loan_amount = st.number_input("Loan Amount ($)", 0)
+income = st.number_input("Applicant Income ($)", value=None)
+credit_score = st.number_input("Credit Score", value=None)
+dti = st.number_input("DTI Ratio (%)", value=None)
+loans = st.number_input("Existing Loans", value=None)
+loan_amount = st.number_input("Loan Amount ($)", value=None)
 
 if st.button("Predict"):
     with open("model.pkl", "rb") as f:
