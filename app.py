a5bbbd952386f1f82af62a7744a7d91119b37361
Bhoomika bg <bhoomikabg63@gmail.com>
2026-03-29 19:29:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ income       = st.number_input("Applicant Income ($)", 0)
 credit_score = st.number_input("Credit Score", 0)
 dti          = st.number_input("DTI Ratio (%)", 0)
 loans        = st.number_input("Existing Loans", 0)
+loan_amount = st.number_input("Loan Amount ($)", 0)
 
 if st.button("Predict"):
     with open("model.pkl", "rb") as f:
         model = pickle.load(f)
-    result = model.predict([[income, credit_score, dti, loans]])[0]
+    result = model.predict([[income, credit_score, dti, loans, loan_amount]])[0]
     st.success("✅ Approved") if result == 1 else st.error("❌ Rejected")
